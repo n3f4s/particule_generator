19a7970ea2a7e3ade587080d97e2bc1e10af0476
n3f4s <tiv.crb@gmail.com>
2017-11-08 13:01:34 +0100

    add lifetime to particle

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate sdl2;
 mod vec3;
 
 use vec3::Vec3;
+use vec3::unit_vector;
 use std::vec::Vec;
 use std::boxed::Box;
 
@@ -41,13 +42,30 @@ impl Point3 {
 pub struct Particle {
     pub position: Point3,
     pub direction: Vec3,
-    pub alive: bool
+    pub alive: bool,
+    pub lifetime: u64 //tick
     //TODO lifetime of the particle ?
+    //TODO raduis
+    //TODO mass
 }
 
 impl Particle {
+    fn new(p: Point3, d: Vec3) -> Particle {
+        Particle {
+            position: p,
+            direction: d,
+            alive: true,
+            lifetime: 100
+        }
+    }
     fn update(&mut self) {
-        self.position.apply_vec(self.direction)
+        if self.alive {
+            self.position.apply_vec(self.direction/10.0);
+            self.lifetime -= 1;
+            if self.lifetime == 0 {
+                self.alive = false;
+            }
+        }
     }
     fn apply_force(&mut self, f: Vec3) {
         self.direction += f
